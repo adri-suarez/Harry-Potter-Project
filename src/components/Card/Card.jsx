a4cb04953274d5dc6eb40ba73e1eceb60b6492fa
Adrian Suarez <adris1337@gmail.com>
2022-11-10 01:16:14 -0300

    styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,8 +7,8 @@ function Card({name, img, house, actor}) {
     <div className={style.cardsita}>
         <h2>{name}</h2>
         <img src={img} alt="No img available" className='CharacterPhoto'/>
-        <h4>{house?"House: "+house:"No House"}</h4>
-        <p>{actor?"Actor: "+actor:""}</p>
+        <h4>{house?house:"No House"}</h4>
+        <p>{actor?actor:"Unknown Actor"}</p>
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ function Navbar() {
       <Link to='/'>
       <p>Home</p>
       </Link>
+      <h3 className='Title'>Harry Potter Characters</h3>
       <Link to='/about'>
       <p>About</p>
       </Link>
